72e07a45788ecd0f68fba7e828ba63e20e55a51c
AlexejFrank <tt115889@connect.hku.hk>
2021-04-30 20:15:09 +0800

    completed first round of event addition

--- Event/views.py
+++ Event/views.py
@@ -7,8 +7,31 @@ from .forms import PersonalEventForm
 def add_personal_event(request):
     if request.method == "GET":
         casepk = request.GET['pk']
-        case = Case.objects.filter(pk=casepk)
+        case = Case.objects.get(pk=casepk)
         form = PersonalEventForm(case=case)
-        return render(request, "personal_event_creat.html", {"form": form})
+        return render(request, "personal_event_create.html", {"form": form}, pk=casepk)
     else:
-        
+        # form submitted
+        form = PersonalEventForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            casepk = request.GET['pk']
+            case = Case.objects.get(pk=casepk)
+            events = PublicEvent.objects.filter(name=data["name"], location=data["location"], date=data["date"])
+            # accounting for sse
+            event = None
+            if len(events) == 0:
+                event = PublicEvent.objects.create() # not completed
+            else:
+                event = events[0]
+                num = events[0].number_of_cases + 1
+                events[0].update(number_of_cases=num)
+            PersonalEvent.objects.create(**{
+                'event': event, 
+                'case': case, 
+                'description': data['description']
+            })
+            return redirect("./")
+        else:
+            clear_errors = form.errors.get("__all__")
+            return render(request, "personal_event_create.html", {"form": form, "clear_errors": clear_errors})
