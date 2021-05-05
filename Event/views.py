a5da18faba2910bbff5c64b2778e0f19c9c948c1
hosuiting <u3556933@connect.hku.hk>
2021-05-05 15:04:01 +0800

    added API retrieve

--- Event/views.py
+++ Event/views.py
@@ -3,6 +3,8 @@ from .models import PersonalEvent, PublicEvent
 from Case.models import Case
 from .forms import PersonalEventForm
 from django.contrib.auth.decorators import login_required
+import json
+import requests
 # Create your views here.
 
 @login_required(login_url = "login" )
@@ -23,11 +25,23 @@ def add_personal_event(request, pk):
             events = PublicEvent.objects.filter(name=data["name"], location=data["location"], date=data["date"])
             # accounting for sse
             event = None
+            ### Retrive data from api
+            try:
+                response = requests.get('https://geodata.gov.hk/gs/api/v1.0.0/locationSearch?','q=' + data["location"])
+                raw_data = {}
+                raw_data = response.json()
+                print(raw_data[0])
+            except:
+                return render(request, "personal_event_create.html", {"form": form})
+            ###
             if len(events) == 0:
                 add_public_event = {
                     'name': data['name'], 
                     'location': data["location"], 
                     'date': data["date"], 
+                    'address' : raw_data[0]['addressEN'],
+                    'xCoord' : raw_data[0]['x'],
+                    'yCoord' : raw_data[0]['y'],
                     'number_of_cases': 1
                 }
                 event = PublicEvent.objects.create(**add_public_event) # not completed
@@ -35,11 +49,12 @@ def add_personal_event(request, pk):
                 event = events[0]
                 num = events[0].number_of_cases + 1
                 PublicEvent.objects.filter(pk=event.pk).update(number_of_cases=num)
+
             add_personal_event = {
-                'event': event, 
-                'case': case, 
-                'description': data['description']
-            }
+                    'event': event, 
+                    'case': case, 
+                    'description': data['description']
+                }
             print(add_personal_event)
             PersonalEvent.objects.create(**add_personal_event)
             return redirect("../../case_detail?Case_Number=" + case.Case_Number)
