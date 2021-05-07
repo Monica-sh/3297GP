3e5f1ad9f40bff04b173d4b0b9ee752e640d0668
AlexejFrank <tt115889@connect.hku.hk>
2021-05-07 16:24:00 +0800

    aaa

--- Case/urls.py
+++ Case/urls.py
@@ -4,9 +4,8 @@ from . import views
 from Case.views import Case_Detail_View
 
 urlpatterns = [
-    path('case_results/',views.ResultView.as_view(),name = 'case_results'),
+    # path('case_results/',views.ResultView.as_view(),name = 'case_results'),
     #path('case_create/',views.CaseCreateView.as_view(),name = 'case_create'),
     path('case_create/', views.case_create_view ,name = 'case_create')
     # path('<uuid:pk>',Case_Detail_View,name = 'case_detail'),
-
 ]
