db6b1681da98e4610b6a21ba73726855c6a13733
hosuiting <u3556933@connect.hku.hk>
2021-05-08 20:34:22 +0800

    update

--- Case/views.py
+++ Case/views.py
@@ -33,34 +33,3 @@ def case_create_view(request):
         return redirect("../../case_detail?Case_Number=" + new_case.Case_Number)
     else:
         return render(request,'Case/templates/Case_form.html',{'form':form})
-
-'''
-class CaseCreateView(View):
-    form_class = CaseForm
-    template_name = 'Case/templates/Case_form.html'
-
-    def get (self,request):
-        return render(request,self.template_name,{'form':self.form_class()})
-    
-    def post (self,request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            new_case = form.save()
-            # new_case = Case.objects.get(pk=new_case_pk)
-            return redirect("../../case_detail?Case_Number=" + new_case.Case_Number)
-        else:
-            return render(request,self.template_name,{'form':form})
-'''
-
-@login_required(login_url = "login")
-def Case_Detail_View(request,pk):
-
-    obj = Case.objects.get(id=pk)
-    event_list = PersonalEvent.objects.filter(case = obj)
-
-    Data = {
-            'case': obj,
-            'event_list': event_list,
-    }
-
-    return render(request,'../templates/case_detail.html', Data) #TODO: Monica connect to result page
